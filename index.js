b47da14100a9633c3b45b181c765f9f64168cf5a
shlomilas1 <94288527+shlomilas1@users.noreply.github.com>
2024-01-08 10:38:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ $(document).one("keydown",function () {
     game();
 });
 
+$(document).one("click",function () {
+    gameStarted = true;
+    $(".btn").on("click" , function () {
+          playSound($(this).attr("id"));
+        }
+    ); 
+    game();
+});
+
 
 
 async function game () {
